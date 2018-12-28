5a245801d57ac2fd8ac85007d9bcca62f54326ac
Shohei Nakagawa <hukusuke1007@gmail.com>
2018-12-28 18:45:05 +0900

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@
 console.log('---- Lesson 1 ----')
 console.log('Hello! world.')
 console.log('------------------')
-
+console.log('')
 // ■ Lesson 2 変数
 console.log('---- Lesson 2 ----')
 let count: number = 1
@@ -12,3 +12,4 @@ let message: String = ``
 message = `Lesson ${count}`
 console.log(message)
 console.log('------------------')
+console.log('')
