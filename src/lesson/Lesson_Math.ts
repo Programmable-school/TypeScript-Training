2c8b7f01feb00c827aed65d244bb183f95b1191a
Shohei Nakagawa <hukusuke1007@gmail.com>
2019-01-21 09:29:44 +0900

    Update Lesson_Math.ts

--- src/lesson/Lesson_Math.ts
+++ src/lesson/Lesson_Math.ts
@@ -13,10 +13,10 @@ export function Lesson_Math() {
   console.log('ceil2', ceil2)     // 6
 
   // ■ 小数点以下を切り捨てて最大整数を返す
-  const foor1 = Math.floor(5.71) 
-  const foor2 = Math.floor(-5.71)
-  console.log('foor1', foor1)     // 5
-  console.log('foor2', foor2)     // -6
+  const floor1 = Math.floor(5.71) 
+  const floor2 = Math.floor(-5.71)
+  console.log('floor1', floor1)   // 5
+  console.log('floor2', floor2)   // -6
 
   // ■ 2乗
   const pow = Math.pow(2, 3)
